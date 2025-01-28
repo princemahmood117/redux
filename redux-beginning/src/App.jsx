f29e0c01043a8bf752540858501a6e596ac01307
princemahmood117 <princemahmood117@gmail.com>
2025-01-28 23:38:21 +0600

    state working

--- redux-beginning/src/App.jsx
+++ redux-beginning/src/App.jsx
@@ -1,23 +1,77 @@
+import { useState } from "react";
+import "./App.css";
+import Counter from "./components/Counter";
+import Stats from "./components/Stats";
 
+const initialCounters = [
+  {
+    id: 1,
+    value: 0,
+  },
+  {
+    id: 2,
+    value: 10,
+  },
 
-import './App.css'
-import Counter from './components/Counter'
-import Stats from './components/Stats'
+];
 
 function App() {
-  
+  const [counters, setCounters] = useState(initialCounters);
+
+
+  // to convert an array into a single value, we use reduce
+
+  const totalCount = counters.reduce((sum,current)=> sum+current.value,0 )
+
+  const handleIncrement = (counterId) => {
+
+    const updatedCounters = counters.map(counter => {
+      if(counter.id === counter.id) {
+        return {
+          ...counter,
+          value : counter.value + 1
+        }
+      } 
+      else {
+        return counter
+      }
+    })
+    setCounters(updatedCounters)
+  }
+
+
+  const handleDecrement = (counterId) => {
+    const updatedCounters = counters.map(counter => {
+      if(counter.id === counter.id) {
+        return {
+          ...counter,
+          value : counter.value - 1
+        }
+      } 
+      else {
+        return counter
+      }
+    })
+    setCounters(updatedCounters)
+  }
 
   return (
-    <div className='h-screen p-10 bg-gray-100 text-slate-700'>
-      <h1 className='max-w-md mx-auto text-center text-2xl font-bold'>Simple Counter application</h1>
+    <div className="h-screen p-10 bg-gray-100 text-slate-700">
+      <h1 className="max-w-md mx-auto text-center text-2xl font-bold">
+        Simple Counter application
+      </h1>
 
-      <div className='max-w-md mx-auto mt-10 space-y-5'>
-        <Counter></Counter>
-        <Counter></Counter>
-        <Stats totalCount={10}></Stats>
+      <div className="max-w-md mx-auto mt-10 space-y-5">
+        {/* <Counter></Counter>
+        <Counter></Counter> */}
+
+        {
+          counters.map(counter => <Counter key={counter.id} count={counter.value} onIncrement={()=> handleIncrement(counter.id)} onDecrement={()=> handleDecrement(counter.id)}></Counter>)
+        }
+        <Stats totalCount={totalCount}></Stats>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- redux-beginning/src/components/Counter.jsx
+++ redux-beginning/src/components/Counter.jsx
@@ -1,26 +1,26 @@
-import { useState } from "react";
+// import { useState } from "react";
 import Count from "./Count";
 import Button from "./Button";
 
-const Counter = () => {
+const Counter = ({count,onIncrement,onDecrement}) => {
 
-    const [count,setCount] = useState(0) 
+    // const [count,setCount] = useState(0) 
 
-    const handleIncrement = () => {
-        setCount((prevCount) => prevCount + 1);
-    }
+    // const handleIncrement = () => {
+    //     setCount((prevCount) => prevCount + 1);
+    // }
 
-    const handleDecrement = () => {
-        setCount((prevCount) => prevCount - 1);
-    }
+    // const handleDecrement = () => {
+    //     setCount((prevCount) => prevCount - 1);
+    // }
 
     return (
         <div className="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow">
             <Count count={count}></Count>
 
             <div className="flex space-x-3">
-                <Button handler={handleIncrement}>Increment</Button>
-                <Button type="danger" handler={handleDecrement}>Decrement</Button>
+                <Button handler={onIncrement}>Increment</Button>
+                <Button type="danger" handler={onDecrement}>Decrement</Button>
             </div>
         </div>  
     );
